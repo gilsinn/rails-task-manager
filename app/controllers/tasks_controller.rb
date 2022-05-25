87a0601d41174a66ec89d6ebfb3f7adb13268b95
Gil Singh <gilsinghsg@gmail.com>
2022-05-25 15:58:58 +0800

    List is done

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,7 +4,6 @@ class TasksController < ApplicationController
     @tasks = Task.all
   end
 
-
   def show
     @task = Task.find(params[:id])
   end
@@ -17,9 +16,27 @@ class TasksController < ApplicationController
     @task = Task.new(task_params)
     @task.save
 
-    redirect_to task_path(@task)
+    redirect_to tasks_path(@task)
   end
 
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+
+    redirect_to tasks_path(@task)
+  end
+
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+
+    redirect_to tasks_path(@task)
+  end
 
   private
 
